12b8f537228972cafa9c6c9649fdcafc0c86bbc8
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-13 01:41:55 +0700

    feat: add states

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,22 @@
 import style from "./Button.module.scss";
 
-import { Paths } from "../../entities";
+import add from "../../assets/Images/Add.png";
+import search from "../../assets/Images/Search.png";
 
 interface ButtonProps {
+  onClick: () => void;
   type?: {
     search?: boolean;
     add?: boolean;
+  };
+  width?: {
+    short?: boolean;
+    wide?: boolean;
+  };
+  color?: {
+    light?: boolean;
+    dark?: boolean;
+    unset?: boolean;
   };
 }
 
@@ -13,17 +24,31 @@ export default function Button(props: ButtonProps) {
   function getButtonImg(): string {
     if (props.type) {
       if (props.type.search) {
-        return Paths.search;
+        return search;
       }
       if (props.type.add) {
-        return Paths.add;
+        return add;
       }
     }
     return "";
   }
 
+  const buttonClassNames = [
+    style.button,
+    props.width?.short ? style.short : "",
+    props.width?.wide ? style.wide : "",
+    props.color?.light ? style.light : "",
+    props.color?.dark ? style.dark : "",
+    props.color?.unset ? style.unset : "",
+  ].join(" ");
+
+  function handleClcik(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    props.onClick();
+  }
+
   return (
-    <button className={style.button__wrapper}>
+    <button className={buttonClassNames} onClick={handleClcik}>
       <img src={getButtonImg()} className={style.button__img} />
     </button>
   );
