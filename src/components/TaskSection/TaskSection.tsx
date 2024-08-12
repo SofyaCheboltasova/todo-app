4af715e6d9c358d6573cf7acfc6d4561fc6861ab
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-13 01:42:34 +0700

    feat: add Modal for a new task

--- src/components/TaskSection/TaskSection.tsx
+++ src/components/TaskSection/TaskSection.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 import style from "./TaskSection.module.scss";
 import common from "../../styles/common.module.scss";
-import { Status } from "../../entities";
+
+import { Priority, Status } from "../../entities";
+import Button from "../Button/Button";
+import Modal from "../Modal/Modal";
 
 interface TaskSectionProps {
   title: Status;
@@ -8,21 +13,44 @@ interface TaskSectionProps {
 }
 
 export default function TaskSection(props: TaskSectionProps) {
+  const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
+
   const colorTheme = {
     [Status.done]: style.done,
     [Status.progress]: style.progress,
     [Status.notStarted]: style.notStarted,
   }[props.title];
 
+  const buttonProps = {
+    onClick: () => setIsModalOpened(true),
+    type: { add: true },
+    width: { wide: true },
+    color: { unset: true },
+  };
+
   return (
-    <section className={`${style.taskSection} ${colorTheme}`}>
-      <h2 className={common.title}>{props.title}</h2>
-      <ul className={style.list}>
-        {props.modals
-          ? props.modals.map((modal, index) => <li key={index}>{modal}</li>)
-          : "Empty list"}
-      </ul>
-    </section>
+    <>
+      {isModalOpened && (
+        <Modal
+          title={""}
+          description={""}
+          priority={Priority.high}
+          status={props.title}
+          dateEnd={new Date(Date.now())}
+          isOpen={true}
+          onClose={() => setIsModalOpened(false)}
+        />
+      )}
+
+      <section className={`${style.taskSection} ${colorTheme}`}>
+        <h2 className={common.title}>{props.title}</h2>
+        <Button {...buttonProps} />
+        <ul className={style.list}>
+          {props.modals &&
+            props.modals.map((modal, index) => <li key={index}>{modal}</li>)}
+        </ul>
+      </section>
+    </>
   );
 }
 
