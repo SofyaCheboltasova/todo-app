18aea1f59f4de8bc7719468035521e9875ea4428
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-12 17:21:28 +0700

    feat: create App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,16 @@
-import "./App.css";
+import TaskSection from "../components/TaskSection/TaskSection";
+import { Status } from "../entities";
+import style from "./App.module.scss";
 
-function App() {}
-
-export default App;
+export default function App() {
+  return (
+    <section className={style.app__wrapper}>
+      <div className={style.app}>
+        <TaskSection title={Status.done} />
+        <TaskSection title={Status.progress} />
+        <TaskSection title={Status.notStarted} />
+      </div>
+    </section>
+  );
+}
 
