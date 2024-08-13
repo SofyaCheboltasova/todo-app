8abfc9da8bd0750f75cd45ab7fe5f8808536d8dc
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-14 02:18:30 +0700

    refactor: change props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import style from "./App.module.scss";
 import { useState } from "react";
 
-import TaskSection from "./components/TaskSection/TaskSection";
+import style from "./App.module.scss";
+
 import { Status } from "./entities";
 import { getTasksFromStorage } from "./utils";
-import Modal, { ModalProps } from "./components/Modal/Modal";
+import { ModalProps } from "./components/Modal/Modal";
+import TaskSection from "./components/TaskSection/TaskSection";
 
 export default function App() {
   const [doneTasks] = useState<ModalProps[] | []>(
@@ -17,40 +18,14 @@ export default function App() {
     getTasksFromStorage(Status.notStarted)
   );
 
-  function getModalSaved(task: ModalProps): React.ReactElement {
-    const { title, description, priority, status, dateEnd } = task;
-    return (
-      <Modal
-        title={title}
-        description={description}
-        priority={priority}
-        status={status}
-        dateEnd={dateEnd}
-        isOpen={false}
-      />
-    );
-  }
+  console.error(doneTasks);
 
   return (
     <section className={style.app__wrapper}>
       <div className={style.app__sections}>
-        <TaskSection
-          title={Status.done}
-          modals={doneTasks && doneTasks.map((task) => getModalSaved(task))}
-        />
-        <TaskSection
-          title={Status.progress}
-          modals={
-            progressTasks && progressTasks.map((task) => getModalSaved(task))
-          }
-        />
-        <TaskSection
-          title={Status.notStarted}
-          modals={
-            notStartedTasks &&
-            notStartedTasks.map((task) => getModalSaved(task))
-          }
-        />
+        <TaskSection title={Status.done} modals={doneTasks} />
+        <TaskSection title={Status.progress} modals={progressTasks} />
+        <TaskSection title={Status.notStarted} modals={notStartedTasks} />
       </div>
     </section>
   );
