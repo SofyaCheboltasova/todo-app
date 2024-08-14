843d78ec805bb7975ec3e7e11b14b4808cde4da3
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-15 03:39:36 +0700

    refactor: update eventHandlers and styles for Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,48 +1,67 @@
 import { useEffect, useRef, useState } from "react";
 
 import style from "./Input.module.scss";
+import { getRefactoredDate } from "../../utils";
 
 interface InputProps {
   value: string;
+  type: "text" | "date";
   onSubmit: (value: string) => void;
   largeText?: boolean;
   darkText?: boolean;
 }
 
 export default function Input(props: InputProps) {
-  const [value, setValue] = useState(props.value);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [value, setValue] = useState<string>(props.value);
+  const textareaRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setValue(props.value);
+  }, [props.value]);
 
   useEffect(() => {
     const textareaElement = textareaRef.current;
-    if (textareaElement) {
+    textareaElement &&
       textareaElement.addEventListener("keydown", handleKeyDown);
-    }
-  }, [value, props]);
 
-  function capitalizeLetter() {
+    return () => {
+      textareaElement &&
+        textareaElement.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [value]);
+
+  function getStyleClass(): string {
+    const { largeText, darkText } = props;
+    return `${largeText && style["large"]} ${darkText && style["dark"]}`;
+  }
+
+  function capitalizeLetter(): string {
     return value.charAt(0).toUpperCase() + value.slice(1);
   }
 
-  function handleKeyDown(e: KeyboardEvent) {
+  function handleKeyDown(e: KeyboardEvent): void {
     if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      props.onSubmit(capitalizeLetter() || props.value);
+      if (props.type === "text") {
+        props.onSubmit(capitalizeLetter());
+      }
       textareaRef.current?.blur();
     }
   }
 
-  function handleBlur() {
-    props.onSubmit(capitalizeLetter() || props.value);
+  function handleBlur(): void {
+    if (props.type === "date") {
+      props.onSubmit(getRefactoredDate(value));
+    }
     textareaRef.current?.blur();
   }
 
   return (
-    <textarea
+    <input
       ref={textareaRef}
       value={value}
+      type={props.type}
       placeholder={"Start typing"}
-      className={`${style.input} ${props.largeText && style["large"]} ${props.darkText && style["dark"]}`}
+      className={`${style.input} ${getStyleClass()}`}
       onChange={(e) => setValue(e.target.value)}
       onBlur={handleBlur}
     />
