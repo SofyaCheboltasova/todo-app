34b69e93b5e25b7e9bddf1763105295907d5cab4
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-15 03:43:30 +0700

    refactor: update App's content

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,74 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import style from "./App.module.scss";
 
-import { Status } from "./entities";
-import { getTasksFromStorage } from "./utils";
+import { Priority, Status } from "./entities";
+import {
+  getRefactoredDate,
+  getStatusTasksFromStorage,
+  saveTaskToStorage,
+} from "./utils";
 import { ModalProps } from "./components/Modal/Modal";
 import TaskSection from "./components/TaskSection/TaskSection";
 
 export default function App() {
-  const [doneTasks] = useState<ModalProps[] | []>(
-    getTasksFromStorage(Status.done)
+  const [doneTasks, setDoneTasks] = useState<ModalProps[]>(
+    getStatusTasksFromStorage(Status.done)
   );
-  const [progressTasks] = useState<ModalProps[] | []>(
-    getTasksFromStorage(Status.progress)
+  const [progressTasks, setProgressTasks] = useState<ModalProps[]>(
+    getStatusTasksFromStorage(Status.progress)
   );
-  const [notStartedTasks] = useState<ModalProps[] | []>(
-    getTasksFromStorage(Status.notStarted)
+  const [notStartedTasks, setNotStartedTasks] = useState<ModalProps[]>(
+    getStatusTasksFromStorage(Status.notStarted)
   );
 
-  console.error(doneTasks);
+  function updateLists(): void {
+    setDoneTasks([...getStatusTasksFromStorage(Status.done)]);
+    setProgressTasks([...getStatusTasksFromStorage(Status.progress)]);
+    setNotStartedTasks([...getStatusTasksFromStorage(Status.notStarted)]);
+  }
+
+  function getDefaultTask(status: Status): ModalProps {
+    const defaultTask: ModalProps = {
+      id: uuidv4(),
+      title: "",
+      description: "",
+      priority: Priority.high,
+      status: status,
+      dateEnd: getRefactoredDate(),
+      isOpen: false,
+    };
+    return defaultTask;
+  }
+
+  function addNewTask(status: Status): void {
+    const newTask = getDefaultTask(status);
+    saveTaskToStorage(newTask);
+    updateLists();
+  }
 
   return (
     <section className={style.app__wrapper}>
       <div className={style.app__sections}>
-        <TaskSection title={Status.done} modals={doneTasks} />
-        <TaskSection title={Status.progress} modals={progressTasks} />
-        <TaskSection title={Status.notStarted} modals={notStartedTasks} />
+        <TaskSection
+          sectionName={Status.done}
+          tasks={doneTasks}
+          onAddTask={addNewTask}
+          onUpdate={updateLists}
+        />
+        <TaskSection
+          sectionName={Status.progress}
+          tasks={progressTasks}
+          onAddTask={addNewTask}
+          onUpdate={updateLists}
+        />
+        <TaskSection
+          sectionName={Status.notStarted}
+          tasks={notStartedTasks}
+          onAddTask={addNewTask}
+          onUpdate={updateLists}
+        />
       </div>
     </section>
   );
