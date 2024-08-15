0d101e4d9c3ba2091beae1a1d17f787a05755b1c
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-15 22:26:17 +0700

    fix: add styles file to build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import "./styles/common.module.scss";
+import './index.module.scss'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
